86783fd6d37857941edd875e6793705418f659dd
Lovelace Rover <none@none.none>
2017-06-14 09:48:37 -0700

    Removed unused angular PID parameters.

--- src/bridge/cfg/params.py
+++ src/bridge/cfg/params.py
@@ -12,11 +12,4 @@ gen.add("linear_db", double_t, 0, "Deadband for linear velocity.", 0, 0, 128)
 gen.add("linear_st", double_t, 0, "Stiction for linear velocity.", 0, 0, 128)
 gen.add("linear_wu", double_t, 0, "Windup limit linear velocity.", 1024, 0, 65535)
 
-gen.add("angular_Kp", double_t, 0, "Kp for angular velocity.", 0.1, 0, 1)
-gen.add("angular_Ki", double_t, 0, "Ki for angular velocity.", 0, 0, 1)
-gen.add("angular_Kd", double_t, 0, "Kd for angular velocity.", 0, 0, 1)
-gen.add("angular_db", double_t, 0, "Deadband for angular velocity.", 0, 0, 128)
-gen.add("angular_st", double_t, 0, "Stiction for angular velocity.", 0, 0, 128)
-gen.add("angular_wu", double_t, 0, "Windup limit angular velocity.", 1024, 0, 65535)
-
 exit(gen.generate(PACKAGE, "config", "Drive"))
