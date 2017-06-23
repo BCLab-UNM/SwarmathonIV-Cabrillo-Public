3bb5f57272c9216020ff655a072ec563f1b96a98
Mike Matera <matera@lifealgorithmic.com>
2017-06-23 14:50:52 -0700

    No defaults in the parameter defs.

--- src/bridge/cfg/params.py
+++ src/bridge/cfg/params.py
@@ -6,12 +6,12 @@ from dynamic_reconfigure.parameter_generator_catkin import *
 gen = ParameterGenerator()
 pid = gen.add_group("PID")
 
-pid.add("scale", double_t, 0, "Scale factor for PID coefficients.", 0.0001, 0, 1000)
-pid.add("Kp", double_t, 0, "Kp for wheel velocity.", 0.1, 0, 1)
+pid.add("scale", double_t, 0, "Scale factor for PID coefficients.", 0, 0, 1000)
+pid.add("Kp", double_t, 0, "Kp for wheel velocity.", 0, 0, 1)
 pid.add("Ki", double_t, 0, "Ki for wheel velocity.", 0, 0, 1)
 pid.add("Kd", double_t, 0, "Kd for wheel velocity.", 0, 0, 1)
 pid.add("db", double_t, 0, "Deadband for wheel velocity.", 0, 0, 128)
 pid.add("st", double_t, 0, "Stiction for wheel velocity.", 0, 0, 128)
-pid.add("wu", double_t, 0, "Windup limit wheel velocity.", 1024, 0, 65535)
+pid.add("wu", double_t, 0, "Windup limit wheel velocity.", 0, 0, 65535)
 
 exit(gen.generate(PACKAGE, "config", "Drive"))
