b76067a3f7c65a5438bc27eb5682ee2e132439b1
Mike Matera <matera@lifealgorithmic.com>
2017-07-01 08:23:42 -0700

    Fix launch to make rover selectable.

--- misc/deploy_host.sh
+++ misc/deploy_host.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+if [ -z "$1" ]; then
+    echo "usage: $0 <rovername>"
+    exit -1
+fi
+
 # Build deploy image
 (
     cd ~/
-    tar -cf - rover_workspace/camera_info rover_workspace/launch rover_workspace/misc rover_workspace/src rover_workspace/*.sh | ssh robot@lovelace tar -xvf -    
+    tar -cf - rover_workspace/camera_info rover_workspace/launch rover_workspace/misc rover_workspace/src rover_workspace/*.sh | ssh robot@$1 tar -xvf -    
 )
 
-ssh -t robot@lovelace /home/robot/rover_workspace/misc/deploy_rover.sh
-
+ssh -t robot@$1 /home/robot/rover_workspace/misc/deploy_rover.sh
