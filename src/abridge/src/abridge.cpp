6fab0ffd96498a7cbc1625d301786ec391b93845
gmfricke <matthew@fricke.co.uk>
2017-08-21 20:23:11 -0600

    Initialised left and right
    The code was missing left and right initialization. The indentation for the new code was fixed to meet the style guide.

--- src/abridge/src/abridge.cpp
+++ src/abridge/src/abridge.cpp
@@ -222,32 +222,35 @@ void driveCommandHandler(const geometry_msgs::Twist::ConstPtr& message) {
 
   if (currentMode == 1) //manual control
   {
-	yawError[0] *= 255/Kpy;
-	velError[0] = linearSpeed * 255/Kpv; //scale values between -255 and 255;
+    float left = (message->linear.x); //target linear velocity in meters per second
+    float right = (message->angular.z); //angular error in radians
 	  
-	float linear = left * 255;
- 	float angular = right * 255; //scale values between -255 and 255;
+    yawError[0] *= 255/Kpy;
+    velError[0] = linearSpeed * 255/Kpv; //scale values between -255 and 255;
+	   
+    float linear = left * 255;
+    float angular = right * 255; //scale values between -255 and 255;
  
- 	left = linear - angular;
- 	right = linear + angular;
+    left = linear - angular;
+    right = linear + angular;
  
- 	if (left >255)
- 	{
- 	  left = 255;
-        }
-         else if (left < -255)
-        {
-           left = -255;
-  	}
+    if (left >255)
+    {
+      left = 255;
+    }
+    else if (left < -255)
+    {
+      left = -255;
+    }
  
- 	if (right >255)
- 	{
- 	  right = 255;
-        }
-        else if (right < -255)
-        {
-           right = -255;
-  	}
+    if (right >255)
+    {
+      right = 255;
+    }
+    else if (right < -255)
+    {
+      right = -255;
+    }
   }
   else //auto control
   {
