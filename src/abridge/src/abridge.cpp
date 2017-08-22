a71c1e2b03845468712ba8eab5c145f209f3cb5b
gmfricke <matthew@fricke.co.uk>
2017-08-21 20:02:56 -0600

    Fixed manual drive control
    Mapping manual inputs to forward vs turning commands has been fixed
    and is now implemented correctly.

--- src/abridge/src/abridge.cpp
+++ src/abridge/src/abridge.cpp
@@ -224,6 +224,30 @@ void driveCommandHandler(const geometry_msgs::Twist::ConstPtr& message) {
   {
 	yawError[0] *= 255/Kpy;
 	velError[0] = linearSpeed * 255/Kpv; //scale values between -255 and 255;
+	  
+	float linear = left * 255;
+ +	float angular = right * 255; //scale values between -255 and 255;
+ +
+ +	left = linear - angular;
+ +	right = linear + angular;
+ +
+ +	if (left >255)
+ +	{
+ +	  left = 255;
+ +        }
+ +        else if (left < -255)
+ +        {
+ +          left = -255;
+ + 	}
+ +
+ +	if (right >255)
+ +	{
+ +	  right = 255;
+ +        }
+ +        else if (right < -255)
+ +        {
+ +          right = -255;
+ + 	}
   }
   else //auto control
   {
