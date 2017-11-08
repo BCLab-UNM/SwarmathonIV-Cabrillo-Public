fe5782d56a65ff2bf0103a5903c4bd7a4286cb4c
Carter Frost <carter90@gmail.com>
2017-11-08 13:16:21 -0800

    Added autodicovery and cleanup

--- misc/roboBuild.sh
+++ misc/roboBuild.sh
@@ -4,8 +4,7 @@
 logfile=/tmp/RoboLog`date +20%y.%m.%d.%H.%M.%S`.txt
 echo Logging stated sent to $logfile |tee logfile
 
-#need a path for catkin?
-catkin build && (echo "Build Successful `date`" |tee $logfile; notify-send "Build Successful `date`") || (echo "Build Failed `date`"|tee $logfile; notify-send "Build Failed `date`"; exit)
+#catkin build && (echo "Build Successful `date`" |tee $logfile; notify-send "Build Successful `date`") || (echo "Build Failed `date`"|tee $logfile; notify-send "Build Failed `date`"; exit)
 
 robot=$(yad --title="Choose a Robot to deploy to" --list --editable --width=200 --height=150 --column="Name" --column="IP" "lovelace" "1" "maverick" "192.168.1.134" "element618" "192.168.1.139")
 echo "user selected $robot" |tee $logfile
@@ -15,5 +14,6 @@ robotIP=`echo $robot | cut -d"|" -f2`
 ping -q -c 2 `echo $robotIP | cut -d"|" -f2` -s 1 &>/dev/null && (echo "Robot is responsive"|tee $logfile; notify-send "Robot is responsive") || (echo "unable to find robot on network at $robotIP"|tee $logfile; notify-send "unable to find robot on network at $robotIP"; exit) 
 
 echo "Transferring built files to $robotName"|tee $logfile; notify-send "Transferring built files to $robotName"
-scp -r ../devel robot@$robotIP:~/rover_workspace/devel 
-#future change to something like rsync -aux ../devel robot@$robotIP:~/rover_workspace/devel
+
+./misc/deploy_host.sh $robotName
+
