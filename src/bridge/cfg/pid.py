b551f7070dc6b2e83fc9bb5a5ff979c6a38bb567
Mike Matera <matera@lifealgorithmic.com>
2017-11-16 18:48:23 -0800

    Tune parameters and a little bug fixing.

--- src/bridge/cfg/pid.py
+++ src/bridge/cfg/pid.py
@@ -12,7 +12,7 @@ pid.add("Ki", double_t, 0, "Ki for wheel velocity.", 0, 0, 1)
 pid.add("Kd", double_t, 0, "Kd for wheel velocity.", 0, 0, 1)
 pid.add("db", double_t, 0, "Deadband for wheel velocity.", 0, 0, 128)
 pid.add("st", double_t, 0, "Stiction for wheel velocity.", 0, 0, 128)
-pid.add("wu", double_t, 0, "Windup limit wheel velocity.", 0, 0, 65535)
+pid.add("wu", double_t, 0, "Windup limit wheel velocity.", 0, 0, 2**32)
 pid.add("ff", double_t, 0, "Feed forward slope.", 0, 0, 65535)
 
 exit(gen.generate(PACKAGE, "pid_dynconfig", "pid"))
