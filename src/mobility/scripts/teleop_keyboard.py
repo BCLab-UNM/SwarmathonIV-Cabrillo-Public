2371dd40f1622e068418f1e6c1edc3eb19366c4d
darrenchurchill <darrenmchurchill@gmail.com>
2018-01-01 00:55:20 -0800

    Drive parameters are now get and set through parameter server using dynamic reconfigure.

--- src/mobility/scripts/teleop_keyboard.py
+++ src/mobility/scripts/teleop_keyboard.py
@@ -3,8 +3,12 @@
 Modified from ros-teleop/teleop_twist_keyboard.
 https://github.com/ros-teleop/teleop_twist_keyboard
 Uses Swarmie API to drive the rover with keyboard.
+
+Questions/comments:
+- Would be nice to be able to call stop_now() or something to stop
+the rover immediately. Since right now, all obstacles are ignored
+while driving.
 """
-# import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import math
 import sys
 import select
@@ -13,7 +17,7 @@ import textwrap
 import tty
 import rospy
 
-# from geometry_msgs.msg import Twist
+import dynamic_reconfigure.client
 from swarmie_msgs.msg import Obstacle
 from mobility.swarmie import Swarmie
 
@@ -21,9 +25,9 @@ msg = """
 Reading from the keyboard and driving using Swarmie API!
 -----------------    -------------------
 Moving around:       Fingers (use the shift key):
-   u    i    o       U         O
+        i            U         O
    j    k    l       (close)   (open)
-   m    ,    .                  
+        ,                       
 
 Wrist:
 -----------------
@@ -33,10 +37,11 @@ b : down
 
 anything else : stop
 
-q/z : increase/decrease linear speed by 10%
-Q/Z : incread/decrease angular speed by 10%
-w/x : increase/decrease drive distance by 10%
-e/c : increase/decrease turn angle by 10%
+q/z : increase/decrease drive speed by 10%
+Q/Z : increase/decrease reverse speed by 10%
+w/x : incread/decrease turn speed by 10%
+e/c : increase/decrease drive distance by 10%
+r/v : increase/decrease turn angle by 10%
 
 Toggle Obstacles to ignore:
 -----------------
@@ -70,15 +75,27 @@ def get_key():
     return key
 
 
-def params_msg(drive_speed, turn_speed, drive_dist, turn_theta):
-    msg = '''currently:\tdrive speed {}\tturn speed {}
-    drive dist {} turn theta {}'''
+def params_msg(drive_speed, reverse_speed, turn_speed, drive_dist, turn_theta):
+    msg = '''currently:
+    drive speed (q/z): {}
+    reverse speed (Q/Z): {}
+    turn speed (w/x): {}
+    drive dist (e/c): {}
+    turn theta (r/v): {}'''
     return textwrap.dedent(msg.format(
         drive_speed,
+        reverse_speed,
         turn_speed,
         drive_dist,
         turn_theta
     ))
+
+
+def update_params(config):
+    global params
+    params['drive_speed'] = config['DRIVE_SPEED']
+    params['reverse_speed'] = config['REVERSE_SPEED']
+    params['turn_speed'] = config['TURN_SPEED']
 
 
 def main():
@@ -91,7 +108,13 @@ def main():
     swarmie = Swarmie(rovername, anonymous=True)
 
     global settings
+    global params
+    params = {}
+
     settings = termios.tcgetattr(sys.stdin)
+    params['drive_dist'] = 0.5
+    params['turn_theta'] = math.pi / 2
+
 
     # pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
     # rospy.init_node('teleop_keyboard')
@@ -127,26 +150,21 @@ def main():
     param_bindings = {
         'q': ['drive_speed', 1.1],
         'z': ['drive_speed', 0.9],
-        'Q': ['turn_speed', 1.1],
-        'Z': ['turn_speed', 0.9],
-        'w': ['drive_dist', 1.1],
-        'x': ['drive_dist', 0.9],
-        'e': ['turn_theta', 1.1],
-        'c': ['turn_theta', 0.9],
+        'Q': ['reverse_speed', 1.1],
+        'Z': ['reverse_speed', 0.9],
+        'w': ['turn_speed', 1.1],
+        'x': ['turn_speed', 0.9],
+        'e': ['drive_dist', 1.1],
+        'c': ['drive_dist', 0.9],
+        'r': ['turn_theta', 1.1],
+        'v': ['turn_theta', 0.9],
     }
 
-    params = {
-        'drive_speed': rospy.get_param(
-            rovername + '_MOBILITY/DRIVE_SPEED',
-            0.2
-        ),
-        'turn_speed': rospy.get_param(
-            rovername + '_MOBILITY/TURN_SPEED',
-            0.6
-        ),
-        'drive_dist': 0.5,
-        'turn_theta': math.pi/2
-    }
+    param_client = dynamic_reconfigure.client.Client(
+        rovername + '_MOBILITY',
+        config_callback=update_params
+    )
+    server_config = param_client.get_configuration()
     ignore_obstacles = Obstacle.IS_SONAR | Obstacle.IS_VISION
 
     status = 0
@@ -155,6 +173,7 @@ def main():
         print msg
         print params_msg(
             params['drive_speed'],
+            params['reverse_speed'],
             params['turn_speed'],
             params['drive_dist'],
             params['turn_theta']
@@ -175,51 +194,45 @@ def main():
                 claw_bindings[key]()  # call the function at that key
             elif key in param_bindings.keys():
                 params[param_bindings[key][0]] *= param_bindings[key][1]
-                if param_bindings[key] == 'drive_speed':
-                    rospy.set_param(
-                        rovername + '_MOBILITY/DRIVE_SPEED',
-                        param_bindings[key][1]
+                if param_bindings[key][0] == 'drive_speed':
+                    param_client.update_configuration(
+                        {'DRIVE_SPEED': params['drive_speed']}
                     )
-                elif param_bindings[key] == 'turn_speed':
-                    rospy.set_param(
-                        rovername + '_MOBILITY/TURN_SPEED',
-                        param_bindings[key][1]
+                elif param_bindings[key][0] == 'reverse_speed':
+                    param_client.update_configuration(
+                        {'REVERSE_SPEED': params['reverse_speed']}
                     )
+                elif param_bindings[key][0] == 'turn_speed':
+                    param_client.update_configuration(
+                        {'TURN_SPEED': params['turn_speed']}
+                    )
+                # Update params once now to make sure no params were
+                # set to invalid values.
+                server_config = param_client.get_configuration()
+                update_params(server_config)
 
                 print params_msg(
                     params['drive_speed'],
+                    params['reverse_speed'],
                     params['turn_speed'],
                     params['drive_dist'],
                     params['turn_theta']
                 )
-                if (status == 14):
+                if (status == 9):
                     print msg
-                status = (status + 1) % 15
+                status = (status + 1) % 10
             else:
                 swarmie.stop()
                 if (key == '\x03'):
                     break
 
             swarmie.stop()
-            # twist = Twist()
-            # twist.linear.x = x*speed
-            # twist.linear.y = y*speed
-            # twist.linear.z = z*speed
-            # twist.angular.x = 0
-            # twist.angular.y = 0
-            # twist.angular.z = th*turn
-            # pub.publish(twist)
 
     except Exception as e:
         print e
 
     finally:
         swarmie.stop()
-        # twist = Twist()
-        # twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-        # twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
-        # pub.publish(twist)
-
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 
