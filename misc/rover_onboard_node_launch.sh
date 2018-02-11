c2b6c15b3e3e6616211cacc11fbfa3257906e8d0
Mike Matera <matera@lifealgorithmic.com>
2018-02-11 09:31:09 -0800

    Add sketch that does calibration. Make the deployer load Arduino code when starting the rover.

--- misc/rover_onboard_node_launch.sh
+++ misc/rover_onboard_node_launch.sh
@@ -7,6 +7,8 @@ else
     export ROS_MASTER_URI=http://$1:11311
 fi
 
+projdir=$(dirname $0)/..
+
 if udevadm info /dev/ttyACM0 | grep -q Leonardo; then
   swarmie_dev=/dev/ttyACM0
   ublox_dev=/dev/ttyACM1
@@ -15,9 +17,41 @@ else
   ublox_dev=/dev/ttyACM0
 fi
 
-launchfile=$(dirname $0)/../launch/swarmie.launch
+launchfile="$projdir/launch/swarmie.launch"
 if [ -f ./launch/swarmie.launch ]; then
     launchfile=./launch/swarmie.launch
 fi
+
+echo "Loading Arduino code."
+if [ -x ~/arduino-1.*/arduino ]; then
+    arduino=~/arduino-1.*/arduino
+elif [ -x /opt/arduino-1.*/arduino ]; then
+    arduino=/opt/arduino-1.*/arduino
+else
+    echo "Couldn't find an Arduino executable."
+    exit 1
+fi
+
+echo Using Arduino executable: $arduino
+
+if [ -f ./Swarmathon-Arduino/Swarmathon_Arduino/Swarmathon_Arduino.ino ]; then
+    repo=$(realpath ./Swarmathon-Arduino)
+elif [ -f $(catkin locate)/Swarmathon-Arduino/Swarmathon_Arduino/Swarmathon_Arduino.ino ]; then
+    repo=$(catkin locate)
+else
+    echo "Couldn't locate Arduino sketch."
+    exit 1 
+fi
+
+sketch=$repo/Swarmathon_Arduino/Swarmathon_Arduino.ino
+build=$repo/build
+
+if udevadm info /dev/ttyACM0 | grep -q Leonardo; then
+  dev=/dev/ttyACM0
+else
+  dev=/dev/ttyACM1
+fi
+
+$arduino --upload --preserve-temp-files --pref serial.port=$dev --pref build.verbose=1 --pref upload.verbose=1 --pref build.path=$build --pref sketchbook.path=$repo --pref board=leonardo $sketch
 
 roslaunch $launchfile name:=$(hostname) simulation:=False swarmie_dev:=$swarmie_dev ublox_dev:=$ublox_dev
