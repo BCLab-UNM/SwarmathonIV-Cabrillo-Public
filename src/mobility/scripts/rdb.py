9c017137b7239479c5362edb50e0fde57e7f07aa
Carter90 <carter90@gmail.com>
2018-05-25 13:04:15 -0700

    fixed the remap for the camera the april tags now works, put back in autodiscovery in rdb.

--- src/mobility/scripts/rdb.py
+++ src/mobility/scripts/rdb.py
@@ -48,22 +48,22 @@ if __name__ == '__main__' :
     global quiet 
     quiet = False   
     
-#     if len(sys.argv) < 2 :
-#         robolist = []
-#         for topic in rospy.get_published_topics():
-#             if topic[1] == 'sensor_msgs/Imu':
-#                 robolist.append(topic[0].split('/')[1])
-#         robolist=list(set(robolist))
-#         if len(robolist) < 1:
-#             print('\033[91m',"No Rovers Detected",'\033[0m')
-#             print ('usage:', sys.argv[0], '<rovername>')
-#             exit (-1)
-#         else: 
-#             rover = robolist[0] #in the future view subscribed topics and to pick a different rover
-#             print("Detected rovers", robolist)
-#             print('\033[92m',"Auto selected:",rover,'\033[0m')
-#     else: 
-#         rover = rospy.get_namespace()
+    if len(sys.argv) < 2 :
+         robolist = []
+         for topic in rospy.get_published_topics():
+             if topic[1] == 'sensor_msgs/Imu':
+                 robolist.append(topic[0].split('/')[1])
+         robolist=list(set(robolist))
+         if len(robolist) < 1:
+             print('\033[91m',"No Rovers Detected",'\033[0m')
+             print ('usage:', sys.argv[0], '<rovername>')
+             exit (-1)
+         else: 
+             rover = robolist[0] #in the future view subscribed topics and to pick a different rover
+             print("Detected rovers", robolist)
+             print('\033[92m',"Auto selected:",rover,'\033[0m')
+    else: 
+         rover = rospy.get_namespace()
 
     rover = rospy.get_namespace()
     print("rospy.get_namespace()", rover)
