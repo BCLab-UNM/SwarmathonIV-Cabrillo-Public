f8bd1862f16adcf99e85f98174530a5f867b4602
Mike Matera <matera@lifealgorithmic.com>
2018-12-07 08:19:20 -0800

    Add shell scripting to do robot selection when testing behaviors and running the debugger.

--- run.sh
+++ run.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
-if [ -d "$(dirname $0)/install" ]; then 
-    source "$(dirname $0)/install/setup.bash"
-else
+if [ -f "$(dirname $0)/devel/setup.bash" ]; then
     source "$(dirname $0)/devel/setup.bash"
-fi
-
-#Delete the rqt cache - can take 24 hours for changes in the UI
-# to show up otherwise
-rm ~/.config/ros.org/rqt_gui.ini
-
-defworld=~/rover_workspace/simulation/worlds/powerlaw_targets_example.world
-exe=$(basename $0)
-if [ "$exe" == "dev.sh" ]; then    
-    roslaunch ./launch/rover_gui.launch startsim:=true numrovers:=1 world:=$defworld
-elif [ "$exe" == "prelim.sh" ]; then
-    roslaunch ./launch/rover_gui.launch startsim:=true round:=prelim world:=$defworld
-elif [ "$exe" == "final.sh" ]; then
-    roslaunch ./launch/rover_gui.launch startsim:=true round:=final world:=$defworld
 else
-    roslaunch ./launch/rover_gui.launch "$@"
+    echo "ERROR: You must compile your code before you run this script."
+    echo "ERROR: Run the following command:" 
+    echo ""
+    echo "  catkin build"
+    echo ""
+    exit -1
 fi
 
-# This seems to be necessary to kill rover nodes
-pkill roslaunch
+function get_rover()
+{
+    rovercount=$(rostopic list | grep driveControl | wc -l)
+    if [ "$rovercount" -gt 1 ]; then
+	echo "ERROR: You can't use this script when there are multiple rovers."
+	echo "ERROR: Use rosrun instead."
+	echo ""
+	echo 'ROS_NAMESPACE=/<rovername> rosrun mobility <node> <arguments>'
+	echo ""
+	exit -2
+    elif [ "$rovercount" -eq 0 ]; then
+	echo "ERROR: There are no rovers to connect to! You must start the GUI"
+	echo "ERROR: and either begin a simulation or deploy to a rover."
+	echo ""
+	exit -2
+    fi
+    rover=$(rostopic list | grep driveControl | cut -d/ -f 2)
+}
 
+exe=$(basename $0)
+if [ "$exe" == "run.sh" ]; then    
+    # Delete the rqt cache - can take 24 hours for changes in the UI
+    # to show up otherwise
+    rm ~/.config/ros.org/rqt_gui.ini
+    roslaunch ./launch/rover_gui.launch "$@"
+elif [ "$exe" == "dev.sh" ]; then
+    get_rover
+    echo "Connecting to rover $rover"
+    ROS_NAMESPACE="/$rover" rosrun mobility "$@"
+elif [ "$exe" == "debug.sh" ]; then
+    get_rover
+    echo "Connecting to rover $rover"
+    ROS_NAMESPACE="/$rover" rosrun mobility rdb.py
+fi
