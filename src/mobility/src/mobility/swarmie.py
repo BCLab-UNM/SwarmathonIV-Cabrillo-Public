30cc8c90759889d63d46841f6fd18bb6992555e5
Carter90 <carter90@gmail.com>
2018-12-19 17:20:09 -0800

    Swarmie.py: removal of some old opencv code, brought back PoseWithCovarianceStamped_to_PoseStamped

--- src/mobility/src/mobility/swarmie.py
+++ src/mobility/src/mobility/swarmie.py
@@ -895,6 +895,12 @@ class Swarmie:
         '''
         return rospy.has_param('search_exit_poses')
         
+    def PoseWithCovarianceStamped_to_PoseStamped(PwCS):
+        '''Helper function to convert PoseWithCovarianceStamped to PoseStamped this is to resolve the change with apriltags 2'''
+        ps = PoseStamped()
+        ps.header = PwCS.header
+        ps.pose = PwCS.pose.pose
+        return ps
 
 #
 # Global singleton instance. No code should make a new instance
@@ -906,43 +912,3 @@ class Swarmie:
 # swarmie.drive()
 #
 swarmie = Swarmie()
-
-        FLANN_INDEX_KDTREE = 0
-        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-        search_params = dict(checks = 50)
-
-        flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-        matches = flann.knnMatch(des1,des2,k=2)
-
-        # store all the good matches as per Lowe's ratio test.
-        good = []
-        for m,n in matches:
-            if m.distance < 0.7*n.distance:
-                good.append(m)
-                
-        '''#for testing2 ###############################################
-        from matplotlib import pyplot as plt
-        # Need to draw only good matches, so create a mask
-        matchesMask = [[0,0] for i in xrange(len(matches))]
-
-        # ratio test as per Lowe's paper
-        for i,(m,n) in enumerate(matches):
-            if m.distance < 0.7*n.distance:
-                matchesMask[i]=[1,0]
-
-        draw_params = dict(matchColor = (0,255,0),
-                           singlePointColor = (255,0,0),
-                           matchesMask = matchesMask,
-                           flags = 0)
-        img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-        plt.imshow(img3,),plt.show()
-        '''#end for testing2   ###############################################
-        
-        print("Seen a resource with",len(good)*5,"% confidence")
-        
-        if len(good)>required_matches-1:
-            return(True)
-        else:
-            return(False)
-        
