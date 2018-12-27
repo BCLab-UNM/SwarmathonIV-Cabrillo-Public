3a695df3e28180f591766f038a380e358c9fa8c0
Darren Churchill <darrenmchurchill@gmail.com>
2018-12-26 17:39:45 -0700

    Refactor using two separate start queues.
    Using two queues allows two rovers do to their init behavior at a time. Only rovers on approximately opposite sides of the home ring should be going at once.

--- src/mobility/scripts/coordinate.py
+++ src/mobility/scripts/coordinate.py
@@ -8,16 +8,17 @@ try:
 except ImportError:
     pass
 
-import angles
+import math
 import rospy
 import threading
 
 from nav_msgs.msg import Odometry
+from control_msgs.srv import QueryCalibrationState
 
 from mobility import sync
 from mobility.swarmie import Location
 
-from swarmie_msgs.msg import SwarmieHeading
+from swarmie_msgs.msg import StartQueuePriority
 
 
 coord_lock = threading.Lock()
@@ -35,44 +36,69 @@ class Coordinator(rospy.SubscribeListener):
         super(Coordinator, self).__init__()
 
         self._initial_pose = Location(None)
-        self._initial_headings = []  # Accrue a list of all rovers' initial headings.
-        self._start_queue = []  # Rovers sorted by their initial heading.
+        self._queue_priority = StartQueuePriority()
+        self._start_queues = [[], []]  # Rovers sorted by their priority.
 
         rospy.init_node('coordinate')
 
-        rospy.Subscriber('/initial_heading', SwarmieHeading,
-                         self._initial_heading_cb, queue_size=10)
+        rospy.Subscriber('/start_queue_priorities', StartQueuePriority,
+                         self._insert_to_start_queue, queue_size=10)
 
+        self._wait_for_initial_odometry()
+        self._set_start_queue_priority()
+
+        self.pub = rospy.Publisher(
+            '/start_queue_priorities', StartQueuePriority,
+            subscriber_listener=self, queue_size=10
+        )
+
+    def _wait_for_initial_odometry(self):
+        """Wait until a message comes in from the IMU/Wheel Encoder EKF."""
         while self._initial_pose.Odometry is None and not rospy.is_shutdown():
             # TODO: Should there be a backup if this message doesn't arrive within 10-15 sec?
             try:
                 self._initial_pose.Odometry = rospy.wait_for_message(
-                    'odom/filtered',
-                    Odometry,
-                    timeout=2
+                    'odom/filtered', Odometry, timeout=2
                 )
+
             except rospy.ROSException:
                 rospy.logwarn(rospy.get_namespace().strip('/') +
                               ': timed out waiting for filtered odometry data.')
 
-        self.pub = rospy.Publisher('/initial_heading', SwarmieHeading,
-                                   subscriber_listener=self, queue_size=10)
+    def _set_start_queue_priority(self):
+        """Set this rover's start queue priority and fill out the
+        message fields."""
+        self._queue_priority.rover_name = (
+            self._initial_pose.Odometry.child_frame_id.split('/')[0]
+        )
+
+        initial_heading = self._initial_pose.get_pose().theta
+
+        if initial_heading > 0:
+            self._queue_priority.queue_index = StartQueuePriority.QUEUE_ONE
+            self._queue_priority.priority = math.pi - initial_heading
+        else:
+            self._queue_priority.queue_index = StartQueuePriority.QUEUE_TWO
+            self._queue_priority.priority = abs(initial_heading)
 
     @sync(coord_lock)
-    def _initial_heading_cb(self, msg):
-        # type: (SwarmieHeading) -> None
-        self._initial_headings.append(msg)
-        self._start_queue = sorted(
-            self._initial_headings,
-            key=lambda x: angles.normalize_angle_positive(x.theta),
-            reverse=True
+    def _insert_to_start_queue(self, msg):
+        # type: (StartQueuePriority) -> None
+        """Insert rover into the appropriate start queue and re-sort the queue
+        by priorities.
+        """
+        self._start_queues[msg.queue_index].append(
+            (msg.priority, msg.rover_name)
         )
+        self._start_queues[msg.queue_index].sort(key=lambda tup: tup[0])
 
         log_msg = []
 
-        for rover in self._start_queue:  # type: SwarmieHeading
-            log_msg.append('{}: theta: {}'.format(rover.rover_name,
-                                                  round(rover.theta, 3)))
+        for index, start_queue in enumerate(self._start_queues):
+            log_msg.append('Queue {}: '.format(index + 1))
+            for rover in start_queue:
+                log_msg.append('{}: priority: {}'.format(rover[1],
+                                                         round(rover[0], 3)))
 
         rospy.loginfo('Current start order: ' + str(log_msg))
 
@@ -93,10 +119,7 @@ class Coordinator(rospy.SubscribeListener):
             None
         """
         # rospy.loginfo('New subscriber to: {}'.format(topic_name))
-        msg = SwarmieHeading()
-        msg.rover_name = self._initial_pose.Odometry.child_frame_id.split('/')[0]
-        msg.theta = self._initial_pose.get_pose().theta
-        peer_publish(msg)
+        peer_publish(self._queue_priority)
 
     @sync(coord_lock)
     def peer_unsubscribe(self, topic_name, num_peers):
