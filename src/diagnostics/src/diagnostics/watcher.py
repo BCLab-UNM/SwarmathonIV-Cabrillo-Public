22d191ac36fbb1c19aa1eef09e24a7e583277bac
Mike Matera <maximus@fatboycentral.com>
2019-02-09 13:28:31 -0800

    Replace the C++ diagnostics with a simpler python package.
    The new diagnostics watches selected topics for activity. If no activity
    is found after 10 seconds diagnostics stops the rover and exits the launch

--- src/diagnostics/src/diagnostics/watcher.py
+++ src/diagnostics/src/diagnostics/watcher.py
@@ -2,6 +2,9 @@
 Watch a topic for activity. 
 """
 
+import rospy 
+from rospy.msg import AnyMsg
+
 class TopicWatcher: 
 
     INIT_STATE     = 0 
@@ -12,9 +15,12 @@ class TopicWatcher:
     def __init__(self, topicname, init_time=60, warn_time=5, fail_time=10):
         self._state = TopicWatcher.INIT_STATE
         self._last_t = rospy.get_rostime()
-        self._topic = topicname 
         self._sub = None 
         self._ns = rospy.get_namespace()
+        if not topicname.startswith('/'):
+            self._topic = self._ns + topicname 
+        else:
+            self._topic = topicname             
         self._init_t = init_time 
         self._warn_t = warn_time 
         self._fail_t = fail_time 
@@ -24,38 +30,40 @@ class TopicWatcher:
         self._last_t = rospy.get_rostime()
         
     def check(self):
+        try:
+            init_cutoff = rospy.get_rostime() - rospy.Duration(self._init_t)
+            warn_cutoff = rospy.get_rostime() - rospy.Duration(self._warn_t)
+            fail_cutoff = rospy.get_rostime() - rospy.Duration(self._fail_t)
+        except TypeError as e:
+            # This happens when the system hasn't been running longer than a cutoff duration. 
+            return 
+        
         if self._state == TopicWatcher.INIT_STATE:
-            if self._last_t < rospy.get_rostime() - rospy.Duration(self._init_t):
-                print ('Topic never published. Entering failed state.')
+            if self._last_t < init_cutoff:
                 self._msg = "Topic " + self._topic + " was not published after " + str(self._init_t) + " seconds."
                 self._state = TopicWatcher.FAILED_STATE
             else:
                 topics = rospy.get_published_topics()
                 for topic in topics:
                     topic_name, topic_type = topic
-                    if topic_name == self._ns + self._topic:
+                    if topic_name == self._topic:
                         # Subscribe
-                        print ('Subscribing to', topic)
                         self._sub = rospy.Subscriber(topic_name, AnyMsg, self._callback)
                         self._state = TopicWatcher.ACTIVE_STATE 
                         self._last_t = rospy.get_rostime()
 
         elif self._state == TopicWatcher.ACTIVE_STATE:
-            if self._last_t < rospy.get_rostime() - rospy.Duration(self._warn_t):
-                print ('Entering the warning state.')
+            if self._last_t < warn_cutoff:
                 self._state = TopicWatcher.WARNING_STATE
 
         elif self._state == TopicWatcher.WARNING_STATE:
-            if self._last_t < rospy.get_rostime() - rospy.Duration(self._fail_t):
-                print ('Entering the failed state.')
+            if self._last_t < fail_cutoff:
                 self._state = TopicWatcher.FAILED_STATE
                 self._msg = "Topic " + self._topic + " stopped publishing for " + str(self._fail_t) + " seconds."
-            elif self._last_t >= rospy.get_rostime() - rospy.Duration(self._warn_t):
-                print ('Saw a message, returning to OK state.')
+            elif self._last_t >= warn_cutoff:
                 self._state = TopicWatcher.ACTIVE_STATE
                 
         elif self._state == TopicWatcher.FAILED_STATE:
-            print (self._msg)
             if self._sub is not None:
                 self._sub.unregister()
 
