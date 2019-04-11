b75f74a4fafb28eb267faa40c48736979bc38545
Darren Churchill <darrenmchurchill@gmail.com>
2019-04-11 16:11:00 -0700

    Use Swarmie API call to get the home odom location.

--- src/mobility/src/mobility/behavior/dropoff.py
+++ src/mobility/src/mobility/behavior/dropoff.py
@@ -14,30 +14,13 @@ from mobility.swarmie import swarmie, PathException
 from mobility.behavior.find_home_corner import find_home_corner
 
 
-def find_home_pose():
-    # type: () -> PoseStamped
-    """Find home's pose in the odom frame.
-
-    Returns:
-        The home plate's pose.
-
-    Raises:
-        tf.Exception if the transform fails.
-    """
-    home_origin = PoseStamped()
-    home_origin.header.frame_id = 'home'
-
-    # TODO: what do we do if this raises a tf exception?
-    return swarmie.transform_pose('odom', home_origin)
-
-
 def dropoff():
     """Drive in and drop off a cube."""
-    home_odom = find_home_pose()
+    home_odom = swarmie.get_home_odom_location()
 
     # Move the wrist up so the resource won't hit the home plate.
     swarmie.set_wrist_angle(.3)
-    swarmie.drive_to(home_odom.pose.position, claw_offset=0.5,
+    swarmie.drive_to(home_odom, claw_offset=0.5,
                      ignore=Obstacle.IS_VISION|Obstacle.IS_SONAR)
 
     swarmie.set_wrist_angle(.7)
