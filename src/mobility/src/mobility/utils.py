bacc924a1224f3c6a6b1db84a262bf94a00859fa
Darren Churchill <darrenmchurchill@gmail.com>
2019-04-13 14:56:18 -0700

    Define a function to calculate a block's exact pose.

--- src/mobility/src/mobility/utils.py
+++ src/mobility/src/mobility/utils.py
@@ -5,7 +5,7 @@ preferred. See below link for examples:
 
 https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 """
-from __future__ import print_function
+from __future__ import division, print_function
 
 try:
     from typing import TYPE_CHECKING
@@ -14,6 +14,9 @@ try:
 except ImportError:
     pass
 
+import tf.transformations
+
+from geometry_msgs.msg import Point, PoseStamped, Quaternion
 from nav_msgs.msg import Odometry
 
 from apriltags2to1.msg import AprilTagDetection
@@ -52,3 +55,42 @@ def is_moving(odom, lin_x_thld=0.1, ang_z_thld=0.2):
     """
     return (abs(odom.twist.twist.linear.x) > 0.1
             or abs(odom.twist.twist.angular.z) > 0.2)
+
+
+def block_pose(detection, block_size=0.05):
+    # type: (AprilTagDetection, float) -> PoseStamped
+    """Given a tag detection (id == 0), return the block's pose. The block pose
+    has the same orientation as the tag detection, but it's position is
+    translated to be at the cube's center.
+
+    Args:
+        detection: The AprilTagDetection.
+        block_size: The block's side length in meters.
+    """
+    transform = tf.transformations.concatenate_matrices(
+        tf.transformations.translation_matrix(
+            [detection.pose.pose.position.x,
+             detection.pose.pose.position.y,
+             detection.pose.pose.position.z]
+        ),
+        tf.transformations.quaternion_matrix(
+            [detection.pose.pose.orientation.x,
+             detection.pose.pose.orientation.y,
+             detection.pose.pose.orientation.z,
+             detection.pose.pose.orientation.w]
+        ),
+        tf.transformations.translation_matrix(
+            [0, 0, -block_size / 2]
+        )
+    )
+
+    t = tf.transformations.translation_from_matrix(transform)
+    q = tf.transformations.quaternion_from_matrix(transform)
+
+    ps = PoseStamped()
+    ps.header.frame_id = detection.pose.header.frame_id
+    ps.header.stamp = detection.pose.header.stamp
+    ps.pose.position = Point(*t)
+    ps.pose.orientation = Quaternion(*q)
+
+    return ps
