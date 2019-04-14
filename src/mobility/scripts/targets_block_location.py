d5893d9cc9f81616559de350f1b67a40806ff08a
Darren Churchill <darrenmchurchill@gmail.com>
2019-04-13 17:29:56 -0700

    Use filter_detections() in test script.

--- src/mobility/scripts/targets_block_location.py
+++ src/mobility/scripts/targets_block_location.py
@@ -19,26 +19,44 @@ from apriltags2to1.msg import AprilTagDetection
 from geometry_msgs.msg import Pose, PoseArray
 
 from mobility.swarmie import swarmie
-from mobility.utils import block_pose
+from mobility.utils import block_detection, filter_detections
 
 
-def publish(pub, nearest_block_pub, detections):
-    # type: (rospy.Publisher, rospy.Publisher, List[AprilTagDetection]) -> None
+def publish(blocks_pub,  # type: rospy.Publisher
+            blocks_filtered_pub,  # type: rospy.Publisher
+            nearest_block_pub,  # type: rospy.Publisher
+            detections  # type: List[AprilTagDetection]
+            ):
+    # type: (...) -> None
     """Publish PoseArrays of block poses and the nearest block location."""
     now = rospy.Time.now()
 
-    array = PoseArray()
-    array.header.frame_id = swarmie.rover_name + '/camera_link'
-    array.header.stamp = now
-    array.poses = []
+    blocks = PoseArray()
+    blocks.header.frame_id = swarmie.rover_name + '/camera_link'
+    blocks.header.stamp = now
+    blocks.poses = []
+
+    blocks_filtered = PoseArray()
+    blocks_filtered.header.frame_id = swarmie.rover_name + '/camera_link'
+    blocks_filtered.header.stamp = now
+    blocks_filtered.poses = []
+
+    block_detections = []  # type: List[AprilTagDetection]
 
     for detection in detections:
         try:
-            ps = block_pose(detection, swarmie.block_size)
-            array.poses.append(ps.pose)
+            block_det = block_detection(detection, swarmie.block_size)
+            block_detections.append(block_det)
+            blocks.poses.append(block_det.pose.pose)
 
         except tf.Exception:
             pass
+
+    block_detections = filter_detections(
+        block_detections,
+        dist=swarmie.block_size - 0.01
+    )
+    blocks_filtered.poses = [d.pose.pose for d in block_detections]
 
     nearest = PoseArray()
     nearest.header.frame_id = swarmie.rover_name + '/odom'
@@ -49,19 +67,22 @@ def publish(pub, nearest_block_pub, detections):
         nearest.poses = [Pose(position=block)]
 
     nearest_block_pub.publish(nearest)
-    pub.publish(array)
+    blocks_pub.publish(blocks)
+    blocks_filtered_pub.publish(blocks_filtered)
 
 
 def main():
-    block_pub = rospy.Publisher('targets/blocks',
-                                PoseArray, queue_size=10)
+    blocks_pub = rospy.Publisher('targets/blocks',
+                                 PoseArray, queue_size=10)
+    blocks_filtered_pub = rospy.Publisher('targets/blocks/filtered',
+                                          PoseArray, queue_size=10)
     nearest_block_pub = rospy.Publisher('targets/nearest_block',
                                         PoseArray, queue_size=10)
     rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
         target_tags = swarmie.get_targets_buffer(age=0.5, id=0)  # type: List[AprilTagDetection]
-        publish(block_pub, nearest_block_pub, target_tags)
+        publish(blocks_pub, blocks_filtered_pub, nearest_block_pub, target_tags)
         rate.sleep()
 
 
